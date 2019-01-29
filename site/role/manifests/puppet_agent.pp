f6ce80411e013a87e38db0ad0b82cbbd3caeb4bb
Jeffrey Frelick <jeff@autostructure.io>
2019-01-29 16:27:32 -0500

    more changes

--- site/role/manifests/puppet_agent.pp
+++ site/role/manifests/puppet_agent.pp
@@ -1,4 +1,4 @@
-class role::database_server {
+class role::puppet_agent {
 
   #This role would be made of all the profiles that need to be included to make a database server work
   #All roles should include the base profile
