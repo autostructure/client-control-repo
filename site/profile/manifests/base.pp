7f8784dc57f93bc98c30c03279e46bc952981f36
Jeffrey Frelick <jeff@autostructure.io>
2019-01-29 17:13:09 -0500

    added message to base profile to confirm it ran

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,10 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
+  notify {'Running puppet agent upgrade...':}
+
+  class {'::puppet_agent':
+    package_version => '6.0.4',
+  }
+
 }
